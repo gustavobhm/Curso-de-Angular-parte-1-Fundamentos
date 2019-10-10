f02af925c4b314cdd4ac6c0f92506b898d0a62d9
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-10-10 15:37:25 -0300

    Aula 6 - Melhorando a experiência do usuário

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -5,13 +5,17 @@ import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
     declarations: [
         PhotoComponent,
         PhotoListComponent,
         PhotoFormComponent,
-        PhotosComponent
+        PhotosComponent,
+        FilterByDescription,
+        LoadButtonComponent
     ],
     imports: [
         HttpClientModule,
