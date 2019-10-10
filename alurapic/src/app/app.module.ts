9dc6af52270543f49734dab34b72707c7249291a
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-10-10 09:41:03 -0300

    Aula 4 - Single Page Applications e rotas

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
@@ -9,7 +11,9 @@ import { PhotosModule } from './photos/photos.module';
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    AppRoutingModule,
+    PhotosModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
